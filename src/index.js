4c92beeccfa06cbc2b1d7abc64f5b388c173460d
Kupova Anastatia <aa.kupova@yandex.ru>
2021-10-15 10:44:08 +0300

    Верстка slider__share, подключение плагина  swiper, дополнила файл .eslintrc!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+
 import '../pages/normalize.css';
+import 'swiper/css/bundle';
 import '../pages/index.css';
+
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+console.log(swiper)
+
 // import lottie from 'lottie-web';
 
 // import animation from './assets/animation.json';
@@ -13,4 +37,4 @@ import '../pages/index.css';
 //     autoplay: true,
 //     animationData: animation
 //     // path: '../imgase/lottie-animation/animation.json' // the path to the animation json
-//   });
+//   })

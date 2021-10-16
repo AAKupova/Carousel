ef6cadd38ab528dfb56c08ada6891bd4229088bf
Kupova Anastatia <aa.kupova@yandex.ru>
2021-10-16 19:01:11 +0300

    Верстка slider,  подключение плпгина Swiper

--- src/index.js
+++ src/index.js
@@ -1,28 +1,47 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import { Swiper, Parallax, Mousewheel, Controller } from 'swiper';
 
 import '../pages/normalize.css';
 import 'swiper/css/bundle';
+import 'swiper/css/parallax'
 import '../pages/index.css';
 
-Swiper.use([Navigation, Pagination]);
+Swiper.use([ Parallax, Mousewheel, Controller ]);
 
-const swiper = new Swiper('.swiper', {
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
+const swiperImg = new Swiper('.slider-img', {
+  loop: false,
+  speed: 2500,
+  parallax: true,
+  mousewheel: {
+    invert: false,
   },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
 });
 
-console.log(swiper)
+const swiperText = new Swiper('.slider-text', {
+  loop: false,
+  speed: 2500,
+  mousewheel: {
+    invert: false,
+  },
+});
+
+swiperImg.controller.control = swiperText;
+swiperText.controller.control = swiperImg;
+
+
+
+console.log(swiperImg, swiperText)
+
+const buttonShare = document.querySelector('.botton-share');
+const blockShare = document.querySelector('.slider__share');
+
+buttonShare.addEventListener('click', () => {
+  if(blockShare.classList.contains('hidden')) {
+    blockShare.classList.remove('hidden');
+  } else {
+    blockShare.classList.add('hidden');
+  }
+
+})
 
 // import lottie from 'lottie-web';
 
